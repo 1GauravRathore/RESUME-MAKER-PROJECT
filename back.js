ddc8335b0e9bb12509086bb54c8218f77cebb909
Gaurav Rathore <89125380+1GauravRathore@users.noreply.github.com>
2022-07-20 15:01:34 +0530

    Add files via upload

---
+++ back.js
@@ -0,0 +1,11 @@
+z
+function myFunction() {
+  var x = document.getElementById("myBtn");
+  x.disabled = true;
+}
+
+/* Read
+
+https://css-tricks.com/use-button-element/
+*/
+console.log("file is find");
